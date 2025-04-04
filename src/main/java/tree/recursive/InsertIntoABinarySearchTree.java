d04c5fe39112c02bcfa7e913fe7922cb1b6ca005
Leonardo Wajda <dev.leowajda@tuta.io>
2025-04-04 16:51:59 +0200

    solution(leetcode): add recursive 'Insert into a Binary Search Tree'

---
+++ src/main/java/tree/recursive/InsertIntoABinarySearchTree.java
@@ -0,0 +1,14 @@
+package tree.recursive;
+
+import tree.TreeNode;
+
+public class InsertIntoABinarySearchTree {
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null)   return new TreeNode(val);
+        if (val > root.val) root.right = insertIntoBST(root.right, val);
+        if (val < root.val) root.left = insertIntoBST(root.left, val);
+        return root;
+    }
+
+}
