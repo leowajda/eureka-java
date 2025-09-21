0ac7c85d8bd1e88c4a00413d0e86bf9713523afa
Leonardo Wajda <dev.leowajda@tuta.io>
2025-09-21 10:22:17 +0200

    refactor: method

--- src/main/java/array/recursive/BurstBalloons.java
+++ src/main/java/array/recursive/BurstBalloons.java
@@ -3,15 +3,12 @@ package array.recursive;
 public class BurstBalloons {
 
     public int maxCoins(int[] nums) {
-
-        int n        = nums.length;
-        int[][] memo = new int[n][n];
-
-        return backtrack(nums, memo, 0, n - 1);
+        int n    = nums.length;
+        var memo = new int[n][n];
+        return dfs(nums, memo, 0, n - 1);
     }
 
-    private int backtrack(int[] nums, int[][] memo, int left, int right) {
-
+    private int dfs(int[] nums, int[][] memo, int left, int right) {
         if (left > right)
             return 0;
 
@@ -19,16 +16,14 @@ public class BurstBalloons {
             return memo[left][right];
 
         for (int i = left; i <= right; i++) {
-
             int currBalloon  = nums[i];
             int leftBalloon  = left - 1 == -1 ? 1 : nums[left - 1];
             int rightBalloon = right + 1 == nums.length ? 1 : nums[right + 1];
 
             memo[left][right] = Math.max(
                     memo[left][right],
-                    backtrack(nums, memo, left, i - 1) + (leftBalloon * currBalloon * rightBalloon) + backtrack(nums, memo, i + 1, right)
+                    dfs(nums, memo, left, i - 1) + (leftBalloon * currBalloon * rightBalloon) + dfs(nums, memo, i + 1, right)
             );
-
         }
 
         return memo[left][right];
