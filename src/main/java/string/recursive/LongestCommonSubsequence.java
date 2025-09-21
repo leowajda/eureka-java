61808d57e4991ce394f1d9ee711c7972328efd1f
Leonardo Wajda <dev.leowajda@tuta.io>
2025-09-21 11:39:53 +0200

    refactor: method

--- src/main/java/string/recursive/LongestCommonSubsequence.java
+++ src/main/java/string/recursive/LongestCommonSubsequence.java
@@ -7,10 +7,10 @@ public class LongestCommonSubsequence {
     public int longestCommonSubsequence(String a, String b) {
         int[][] memo = new int[a.length()][b.length()];
         Arrays.stream(memo).forEach(row -> Arrays.fill(row, Integer.MIN_VALUE));
-        return backtrack(a, b, memo, 0, 0);
+        return dfs(a, b, memo, 0, 0);
     }
 
-    private int backtrack(String a, String b, int[][] memo, int ptrA, int ptrB) {
+    private int dfs(String a, String b, int[][] memo, int ptrA, int ptrB) {
         if (ptrA == a.length() || ptrB == b.length())
             return 0;
 
@@ -18,10 +18,10 @@ public class LongestCommonSubsequence {
             return memo[ptrA][ptrB];
 
         if (a.charAt(ptrA) == b.charAt(ptrB))
-            return memo[ptrA][ptrB] = backtrack(a, b, memo, ptrA + 1, ptrB + 1) + 1;
+            return memo[ptrA][ptrB] = dfs(a, b, memo, ptrA + 1, ptrB + 1) + 1;
 
-        int subA = backtrack(a, b, memo, ptrA + 1, ptrB);
-        int subB = backtrack(a, b, memo, ptrA, ptrB + 1);
+        int subA = dfs(a, b, memo, ptrA + 1, ptrB);
+        int subB = dfs(a, b, memo, ptrA, ptrB + 1);
         return memo[ptrA][ptrB] = Math.max(memo[ptrA][ptrB], Math.max(subA, subB));
     }
 
